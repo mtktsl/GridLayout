1fe55630c86c90ad83b0073874a5eb3bd04a3e0c
mtktsl <mtk.007@hotmail.com>
2023-08-07 14:19:24 +0300

    Initial Commit

---
+++ Sources/GridLayout/GridLayout.swift
@@ -0,0 +1,6 @@
+public struct GridLayout {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/GridLayoutTests/GridLayoutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GridLayout
+
+final class GridLayoutTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GridLayout().text, "Hello, World!")
+    }
+}
